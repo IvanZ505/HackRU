c48a0d55227dfa22ea0c0b79b1a980e5c948db2b
Ivan Zheng <zivan7644@gmail.com>
2023-02-25 16:55:01 -0500

    website setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <start v-if="!started" @changeStarted="started = true"></start>
+    <div v-else style="color: white;">ASKJDASL</div>
+</template>
+
+
+<script>
+    import start from './component/Start.vue'
+
+    export default {
+        data() {
+            return {
+                started: false    
+            }
+        },
+        components: {
+            start
+        }
+    }
+</script>
